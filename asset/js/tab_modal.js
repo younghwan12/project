cc7bd0de0eb4a8bfb72daea6048f850a8c9b92d5
fkdldhs8484 <109262134+younghwan12@users.noreply.github.com>
2022-10-07 14:58:15 +0900

    2022-10-07

--- asset/js/tab_modal.js
+++ asset/js/tab_modal.js
@@ -1,16 +1,10 @@
-const loginPopup = document.querySelector(".login__popup");
+
 const tabMenuId = document.querySelector(".id");
 const tabMenuPass = document.querySelector(".pass");
-const loginBtn = document.querySelector(".login_btn");
+
 const close_btn = document.querySelector(".close_btn");
 
 
-loginBtn.addEventListener("click", () => {
-    loginPopup.classList.remove("close");
-    loginPopup.classList.add(".show")
-    loginPopup.classList.remove(".hide")
-
-});
 close_btn.addEventListener("click", () => {
     loginPopup.classList.add("close");
     loginPopup.classList.remove(".show")
